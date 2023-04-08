256913d0ed4a2f00b68c128c21f7eba578ff211f
CiceroLucas <lukasferreidesousa890@gmail.com>
2023-04-08 10:43:41 -0300

    melhorando o projeto e tratando erros

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -1,21 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-//RETORNA TODOS OS PEDIDOS
 router.get('/', (req, res, next) => {
     res.status(200).send({
         msg: 'Retorna os pedidos'
     })
 })
 
-//INSERE UM PEDIDO
 router.post('/', (req, res, next) => {
     res.status(201).send({
         msg: 'O pedido foi criado'
     })
 })
 
-//RETORNA DADOS DE UM PEDIDO
 router.get('/:id_pedidos', (req, res, next) => {
     const id = req.params.id_produto
     res.status(200).send({
@@ -25,7 +22,6 @@ router.get('/:id_pedidos', (req, res, next) => {
     
 })
 
-//EXCLUI UM PEDIDO
 router.delete('/', (req, res, next) => {
     res.status(201).send({
         msg: 'Pedido excluido'
--- server.js
+++ server.js
@@ -2,5 +2,7 @@ const http = require('http')
 const app = require('./app')
 const port = process.env.PORT || 3000
 const server = http.createServer(app)
-server.listen(port)
 
+server.listen(port, () => {
+    console.log(`O servidor está rodando na porta ${port}`)
+})
